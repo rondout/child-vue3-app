414ae4337cfdf9028cf6baf5da1abc779047ff85
shufei.han <shufei.han@gl-inet.com>
2024-08-02 16:14:05 +0800

    chore: 初始化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: shufei.han
+ * @Date: 2024-08-01 16:14:55
+ * @LastEditors: shufei.han
+ * @LastEditTime: 2024-08-01 16:24:59
+ * @FilePath: \child-vue3-app\src\App.vue
+ * @Description: 
+-->
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>This Child Vue3 App v2</h1>
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+/*
+ * @Author: shufei.han
+ * @Date: 2024-08-01 16:14:55
+ * @LastEditors: shufei.han
+ * @LastEditTime: 2024-08-02 16:13:34
+ * @FilePath: \child-vue3-app\vite.config.ts
+ * @Description: 
+ */
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 4002,
+  },
+})
